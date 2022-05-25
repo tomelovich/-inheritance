75c963b9c56a7dbd4b8dd093d06362254b9d6141
tomelovich <tomelovichs@gmail.com>
2022-05-25 11:03:14 +0300

    Выполнено

---
+++ Z-1/Z-1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Z_1
+{
+    class Bus
+    {
+
+        private int numPassengers;
+        private double ticketPrice;
+        private double cost;
+        public Bus()
+        {
+            numPassengers = 0;
+            ticketPrice = 0;
+            cost = 0;           
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Кол-во мест: {0} | Стоимость билета: {1}", numPassengers, ticketPrice);
+        }
+        public virtual void TotalCost()
+        {
+            cost = numPassengers * ticketPrice;
+        }
+        public int NumPassengers { get { return numPassengers; } set { numPassengers = value; } }
+        public double TicketPrice { get { return ticketPrice; } set { ticketPrice = value; } }
+        public double Cost { get { return cost; } set { cost = value; } }
+        public void Input()
+        {
+            Console.Write("Введите кол-во мест- ");
+            numPassengers = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите стоимость билета- ");
+            ticketPrice = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+    class ExpressBus: Bus
+    {
+        public ExpressBus() : base()
+        {            
+            Input();
+        }        
+        public override void TotalCost()
+        {
+            Cost = (TicketPrice + (TicketPrice * 0.05)) * NumPassengers;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Общая стоимость: {0}", Cost);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExpressBus ing = new ExpressBus();
+            ing.TotalCost();
+            ing.GetInfo();
+            Console.ReadLine();
+        }        
+    }
+}
---
+++ Z-1/Z-2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Z_2
+{
+    public class A 
+    {
+        public float a; 
+        public float b;
+        public A() { a = 3f; b = 2.5f; } 
+        public float c 
+        {
+            get { return a / b; }
+            set { c = value; } 
+        }
+    }
+    public class B : A
+    {
+        public float d = 10f; 
+        public int i;
+        public float c2 
+        {
+            get { return a + b + d; }
+            set { c2 = value; }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                A a = new A();
+                B b = new B();
+                Console.WriteLine("Введите число и увидите значения:");
+                Console.WriteLine("1 - полей a,b,d");
+                Console.WriteLine("2 - свойств с,c2");
+                b.i = Convert.ToInt32(Console.ReadLine());
+                switch (b.i)
+                {
+                    case 1: Console.WriteLine("a={0} ", b.a, "b={0} ", b.b, "d={0} ", b.d); break;
+                    case 2: Console.WriteLine("2={0} ", b.c, "c2={0} ", b.c2); break;
+                    default:
+                        Console.WriteLine("Введено неправильное значение!"); break;
+                }
+            }
+        }
+    }
+}
